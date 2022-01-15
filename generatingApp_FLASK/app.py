7cda365839933d4601c752f1c8c06388dc7fcb8b
Mohameosama <moandosama1163@gmail.com>
2022-01-15 23:19:15 +0200

    adding function prams to templet

--- generatingApp_FLASK/app.py
+++ generatingApp_FLASK/app.py
@@ -5,10 +5,12 @@ app.secret_key = "manbearpig_MUDMAN888"
 
 @app.route("/hello")
 def index():
-	flash("Do you Love techno music?, I already Know your answer , So What do you want the name of your song to be?")
+	flash("What do you want the name of your song to be?")
+	flash("How long do you want your song?")
 	return render_template("index.html")
 
 @app.route("/generating", methods=['POST', 'GET'])
 def greeter():
-	flash(str(request.form['name_input']) + ", is generating now for you!")
-	return render_template("index.html")
+	flash(str(request.form['name_input']) + ", is generating now for you! " + str(request.form['min_input']))
+	#fun(request.form['name_input'],request.form['min_input'])
+	return render_template("request.html")
