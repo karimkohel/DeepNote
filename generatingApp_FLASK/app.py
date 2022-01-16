a730e5785e726595cf5049d1efdcf18c71408685
karimkohel <kareemkohel@gmail.com>
2022-01-17 01:27:48 +0200

    finished hall of fame

--- generatingApp_FLASK/app.py
+++ generatingApp_FLASK/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, flash
+import os
 
 app = Flask(__name__)
 app.secret_key = "manbearpig_MUDMAN888"
@@ -15,10 +16,8 @@ def greeter():
 
 @app.route("/main-hall", methods=['POST', 'GET'])
 def fame():
-	# take wavs from file
-	wavs = [
-		"wave1",
-		"wave2",
-		"wave3"
-	]
+	wavs = []
+	wavPaths = [item for item in os.listdir("static") if ".wav" in item]
+	for path in wavPaths:
+		wavs.append({"name": path.replace(".wav", ''), "path":path})
 	return render_template("hall.html", wavs=wavs)
